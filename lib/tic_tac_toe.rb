eeccc546f7f467b65c7b8c11fd8c30c9b89ebc49
Evan Pavone <evanrpavone@gmail.com>
2020-08-07 14:40:55 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -2,7 +2,6 @@ class TicTacToe
   def initialize(board = nil)
     @board = board || Array.new(9, " ")
   end
-end
 
   WIN_COMBINATIONS = [
     [0,1,2],
@@ -38,3 +37,15 @@ end
   def valid_move?(index)
     index.between?(0,8) && !position_taken?(index)
   end
+
+end
+
+
+
+
+
+
+
+
+
+
