3a0bf37746a5ec80f1b0574ab00be1a4646a3269
Evan Pavone <evanrpavone@gmail.com>
2020-08-07 14:41:39 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -13,7 +13,7 @@ class TicTacToe
     [0,4,8],
     [2,4,6]
   ]
-end
+
   def display_board
     puts "#{@board[0]} | #{@board[1]} | #{@board[2]}"
     puts "-----------"
@@ -37,3 +37,5 @@ end
   def valid_move?(index)
     index.between?(0,8) && !position_taken?(index)
   end
+
+end
